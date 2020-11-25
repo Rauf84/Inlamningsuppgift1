d3aa953b29cca32fb68fa4f7e61ce9f20d7b309a
Rauf84 <rauf@karimli.se>
2020-11-25 13:39:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Inlämningsuppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Använder en metod för uppgiften
+        }
+
+        bool tryAgain = true;
+
+        
+    }
+}
+//Inlämningsuppgift 1
+//•	Be användaren att ange ett par operatorer
+//•	Be sedan användaren om ett par termer att använda operatorerna på
+//•	Visa sen ekvationen och resultatet av beräkningen.
+//•	Resultatet skall sen läggas till i en array/List
+//•	Om summan är mindre än 100 skriv ut; ”Less then a hundred”
+//•	Om summan är mer än 100 skriv ut; ”More then a hundred”
+//•	Om summan är lika med 100 skriv ut; ”Cool, now you have a hundred, clap clap”
+//•	Programmets titel i konsolen skall vara modifierat
+//•	Programmet skall upprepa tills användaren själv väljer att avsluta
+//•	Vid avslut skall summan av alla resultat skrivas ut
+//Använd Git för att verisionshantera koden och ladda upp git-repot till din GitHub
+//Programmet måste inte vara felfritt eller buggfritt. Framför allt är det verisionshanteringen som testas. Om två operatorer är för svårt, börja med en och sen lägg på fler om du kan.
+//Exempel: 
+//(‘>’ or ‘$’ används ofta för att notera att något händer i en kommandotolk)
+//> Enter first operator: x
+//> Enter second operator: -
+//> Enter first term: 8 
+//> Enter second term: 6 
+//> Enter third term: 3 
+//> 8 x 6 – 3 = 45
+//> ”The sum is less then a hundred”
+//> Another try?
+//> Thank you for playing.The sum of all round is 221. Bye
